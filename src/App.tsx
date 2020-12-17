47cd98a9f084d49a5368fc810c55ce6bf389297c
Vlad Prodan <vladprodan6@gmail.com>
2020-12-18 01:50:41 +0200

    tests

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,18 @@ function App():JSX.Element {
     return (
         <div className="App">
             <Switch>
-                <Route exact path="/signup" component={SignUp}/>
+                <Route exact path="/" component={SignUp}/>
                 <Route exact path="/login" component={LogIn}/>
                 <Route exact path="/reset" component={ResetPassword}/>
                 <Route exact path="/subjects" component={ChoiceSubjects}/>
                 <div className="d-flex home">
                     <SideBar/>
-                    <Container>
+                    <Container fluid>
                         <Route exact path="/subject/:id" component={Subject}/>
                         <Route exact path="/home" component={Home}/>
                         <Route exact path="/tests" component={Tests}/>
-                        <Route exact path="/test/:id" component={Test}/>
                         <Route exact path="/subject-page" component={Subjects}/>
-
+                        <Route exact path="/test/:id" component={Test}/>
                     </Container>
                 </div>
                 <Redirect to="/signup"/>
--- src/pages/Subjects.tsx
+++ src/pages/Subjects.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { Container, Row, Col, Card, CardBody, CardImg } from "shards-react";
 
 
@@ -15,12 +16,14 @@ function Subjects() {
                 </Row>
                 <Row className="mt-5">
                     <Col lg="4">
-                        <Card>
-                            <CardImg top src="https://place-hold.it/300x200" />
-                            <CardBody className="text-center">
-                                <p>Всі Тести</p>
-                            </CardBody>
-                        </Card>
+                        <Link to="/tests">
+                            <Card>
+                                <CardImg top src="https://place-hold.it/300x200" />
+                                <CardBody className="text-center">
+                                    <p>Всі Тести</p>
+                                </CardBody>
+                            </Card>
+                        </Link>
                     </Col>
 
                     <Col lg="4">
